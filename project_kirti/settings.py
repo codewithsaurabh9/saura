45bd4a78969690f03a2164b962c0a49164b1b584
saurabh <88712236+codewithsaurabh9@users.noreply.github.com>
2024-12-31 00:28:19 +0530

    Initial commitdfffd

--- project_kirti/settings.py
+++ project_kirti/settings.py
@@ -52,7 +52,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+
+  
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # Other middlewares...
 ]
+
+
 
 ROOT_URLCONF = 'project_kirti.urls'
 
